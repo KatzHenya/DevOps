c9a2898ee718018419972ff0bd515823442c2530
Henya-Pshendza <0504144432bb@gmail.com>
2026-08-18 19:08:51 +0300

    Fix GKE subnetwork name

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -3,17 +3,10 @@ resource "google_container_cluster" "autopilot" {
   name     = "kanban-cluster"
   location = var.region
 
-  # Autopilot mode — GCP מנהל את ה-nodes אוטומטית
   enable_autopilot = true
 
   network    = google_compute_network.vpc.name
-  subnetwork = "projects/${var.project_id}/regions/${var.region}/subnetworks/${var.region}"
-
-  # מאפשר גישה פרטית ל-cluster
-  private_cluster_config {
-    enable_private_nodes    = false
-    enable_private_endpoint = false
-  }
+  subnetwork = google_compute_network.vpc.name
 
   deletion_protection = false
 }
